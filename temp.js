8855697e41adad2d2058075c0a2ab89ae6091cb5
beastjk <41422062+beastjk@users.noreply.github.com>
2020-07-04 15:47:34 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    // 'use strict';  
+    var typed = $(".typed");
+  
+    $(function() {
+      typed.typed({
+        strings: ["Money Heist"],
+        typeSpeed: 100,
+        loop: true,
+      });
+    });
+  })
+  
+      
